e0355adfcd05fec0f66a03cc176f92806971e381
kgauzampe <kgauza.mpe@gmail.com>
2020-06-18 14:27:29 +0200

    Updates

---
+++ precache-manifest.73c4a5d208b71f7556c5fc7923db4612.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0030f87cc4dca28a0b9ca048fc414ddf",
+    "url": "/Site/index.html"
+  },
+  {
+    "revision": "6d86f1c931a6d554f897",
+    "url": "/Site/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "1c359a745d110a11bb8f",
+    "url": "/Site/static/css/main.bfd17e5b.chunk.css"
+  },
+  {
+    "revision": "6d86f1c931a6d554f897",
+    "url": "/Site/static/js/2.248bee71.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/Site/static/js/2.248bee71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c359a745d110a11bb8f",
+    "url": "/Site/static/js/main.ba92a939.chunk.js"
+  },
+  {
+    "revision": "b3df52f860ee1601dc16",
+    "url": "/Site/static/js/runtime-main.ec94288a.js"
+  }
+]);
